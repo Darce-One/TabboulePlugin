9946320825e90ec6c4a459a10d9755b381668610
Darce-One <96296082+Darce-One@users.noreply.github.com>
2022-04-13 22:40:38 +0300

    Initial commit

---
+++ Tabbouleh/Source/PluginEditor.cpp
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+TabboulehAudioProcessorEditor::TabboulehAudioProcessorEditor (TabboulehAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+TabboulehAudioProcessorEditor::~TabboulehAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void TabboulehAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void TabboulehAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
